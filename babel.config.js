d2fc90645d8bce0223f49d2aa2afba4d73e711d8
Taisiya <dellesert@gmail.com>
2025-11-25 18:24:48 +0300

    chore: remove unused Tailwind CSS/NativeWind and cleanup
    Removed:
    - Tailwind CSS and NativeWind configuration files
      - tailwind.config.js
      - postcss.config.js
      - nativewind-env.d.ts
    - Migration documentation files (refactoring complete)
      - MIGRATION_PLAN.md
      - MIGRATION_CHEATSHEET.md
      - REFACTORING_CHEATSHEET.md
      - REFACTORING_PROMPT.md
    - Old layer-based architecture files
      - constants/Colors.ts
      - contexts/ThemeContext.tsx
      - docs/API_ERROR_HANDLING.md
      - docs/API_ERROR_HANDLING_QUICKSTART.md
    
    Updated:
    - babel.config.js: removed 'nativewind/babel' plugin
    - babel.config.js: removed obsolete @components and @screens aliases
    - package.json: removed nativewind and tailwindcss dependencies

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,12 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'nativewind/babel',
       [
         'module-resolver',
         {
           root: ['./src'],
           alias: {
             '@': './src',
-            '@components': './src/components',
-            '@screens': './src/screens',
             '@navigation': './src/navigation',
             '@api': './src/api',
             '@services': './src/services',
